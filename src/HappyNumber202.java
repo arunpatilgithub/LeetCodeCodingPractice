9e7375e95f5f2131c7a8c7de3bd2b8c8630c60e7
arunpatil <arunpatilmailbox@gmail.com>
2023-10-06 17:50:52 -0600

    more solutions

---
+++ src/HappyNumber202.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HappyNumber202 {
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+        System.out.println(isHappy(2));
+    }
+
+    private static boolean isHappy(int n) {
+
+        if (n == 1) {
+            return true;
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        while (!visited.contains(n)) {
+
+            if (n == 1) {
+                return true;
+            }
+
+            visited.add(n);
+
+            int tempSum = 0;
+            int tempNum = n;
+            while (tempNum != 0) {
+                int digit = tempNum % 10;
+                digit = digit * digit;
+                tempSum = tempSum + digit;
+                tempNum = tempNum / 10;
+            }
+
+            n = tempSum;
+        }
+
+        return false;
+    }
+
+
+}
