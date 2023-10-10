1b57253240c29216a1175f3414653fa4d856fdb9
arun patil <arunpatilmailbox@gmail.com>
2023-10-10 07:58:09 -0600

    more solutions

--- src/ConstructBinaryTreeFromPreorderInorder105.java
+++ src/ConstructBinaryTreeFromPreorderInorder105.java
@@ -1,34 +1,51 @@
 import util.BinaryTreeFromArray.TreeNode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 
-//FIXME: Incomplete code
+/**
+ * Refer @see <a href="https://www.youtube.com/watch?v=PbPS460rbMo&ab_channel=NikhilLohia"> this video</a>
+ * with great explanation.
+ */
 public class ConstructBinaryTreeFromPreorderInorder105 {
 
     public static void main(String[] args) {
 
-        buildTree(new int[]{3,9,20,15,7}, new int[]{9,3,15,20,7});
+        buildTree(new int[]{8,2,7,1,9,3,6}, new int[]{7,2,1,8,3,9,6});
     }
 
     private static TreeNode buildTree(int[] preorder, int[] inorder) {
 
-        int mid = 0;
-        while (preorder[0] != inorder[mid]) {
-            mid++;
+
+        Map<Integer, Integer> inOrderMap = new HashMap<>();
+
+        for (int i=0; i< inorder.length; i++) {
+            inOrderMap.put(inorder[i],i);
         }
 
-        TreeNode root = new TreeNode(inorder[mid]);
-        Queue<TreeNode> queue = new LinkedList<>();
+        return recurse(preorder, inOrderMap, 0, 0, inorder.length-1);
 
-        queue.add(root);
+    }
 
+    private static TreeNode recurse(int[] preorder, Map<Integer, Integer> inOrderMap, int rootIndex,
+                                    int inOrderLeftBoundary, int inorderRightBoundary) {
 
+        TreeNode root = new TreeNode(preorder[rootIndex]);
 
+        int partitionPosition = inOrderMap.get(preorder[rootIndex]);
+
+        if (partitionPosition > inOrderLeftBoundary) {
+            root.left = recurse(preorder, inOrderMap, rootIndex+1, inOrderLeftBoundary, partitionPosition-1);
+        }
+
+        if (partitionPosition < inorderRightBoundary) {
+            root.right = recurse(preorder, inOrderMap, rootIndex+partitionPosition-inOrderLeftBoundary+1, partitionPosition+1, inorderRightBoundary);
+        }
 
         return root;
-
     }
 
 }
