048f02887a5a43ea74f8b78ee1052e1a670b3656
arunpatil <arunpatilmailbox@gmail.com>
2023-10-11 07:38:42 -0600

    more solutions

--- src/LinkedListCycle141.java
+++ src/LinkedListCycle141.java
@@ -5,9 +5,39 @@ public class LinkedListCycle141 {
 
     public static void main(String[] args) {
 
+        System.out.println(hasCycle(createLinkedListWithCycle(new int[]{3,2,0,-4},1)));
+
     }
 
+
     private static boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != null && fast != null) {
+
+            if (slow == fast) {
+                return true;
+            }
+
+            if (slow.next == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+
+        return false;
+
+    }
+
+    private static boolean hasCycle2(ListNode head) {
 
         Set<ListNode> visited = new HashSet<>();
 
@@ -26,7 +56,7 @@ public class LinkedListCycle141 {
 
     }
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) {
@@ -34,6 +64,33 @@ public class LinkedListCycle141 {
             next = null;
         }
     }
+
+    private static ListNode createLinkedListWithCycle(int[] nums, int pos) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        ListNode cycleStart = null;
+
+        for (int i = 0; i < nums.length; i++) {
+            ListNode newNode = new ListNode(nums[i]);
+            current.next = newNode;
+            current = current.next;
+
+            if (i == pos) {
+                cycleStart = current;
+            }
+        }
+
+        if (cycleStart != null) {
+            current.next = cycleStart;
+        }
+
+        return dummy.next;
+    }
+
 
 
 }
