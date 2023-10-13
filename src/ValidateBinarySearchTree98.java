041f1ca66ccab3be8b810d1e9499833646ff4bb9
arunpatil <arunpatilmailbox@gmail.com>
2023-10-13 10:46:34 -0600

    more solutions

--- src/ValidateBinarySearchTree98.java
+++ src/ValidateBinarySearchTree98.java
@@ -1,31 +1,38 @@
 import util.BinaryTreeFromArray;
 import util.BinaryTreeFromArray.TreeNode;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ValidateBinarySearchTree98 {
 
     public static void main(String[] args) {
 
-        //System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{5,1,4,null,null,3,6})));
-        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{2,1,3})));
+//        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{5,1,4,null,null,3,6})));
+//        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{2,1,3})));
+//        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{1,1})));
+//        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{0,null,-1})));
+        System.out.println(isValidBST(BinaryTreeFromArray.createTree(new Integer[]{2147483647})));
 
     }
 
     private static boolean isValidBST(TreeNode root) {
 
-        return dfs(root, null, null);
+        return recurseTree(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private static boolean dfs(TreeNode node, Integer left, Integer right) {
+    private static boolean recurseTree(TreeNode root, long minValue, long maxValue) {
 
-        if (node == null) {
+        if (root == null) {
             return true;
         }
 
-        if ((left != null && node.val <= left) || (right != null && node.val >= right)) {
+        if (root.val <= minValue || root.val >= maxValue) {
             return false;
         }
 
-        return dfs(node.left, left, node.val) && dfs(node.right, node.val, right);
+        return recurseTree(root.left, minValue, root.val) && recurseTree(root.right, root.val,maxValue);
+
     }
+
 
 }
