5fec7b187e0e0348533416a329a4d399fd8d8bdf
arunpatil <arunpatilmailbox@gmail.com>
2023-10-16 15:44:50 -0600

    more solutions

--- src/NumberOfIslands200.java
+++ src/NumberOfIslands200.java
@@ -33,14 +33,12 @@ public class NumberOfIslands200 {
         int M = grid.length;
         int N = grid[0].length;
 
-        boolean[][] visited = new boolean[M][N];
-
         int islands = 0;
 
         for (int r=0; r<M; r++) {
             for (int c=0; c<N; c++) {
-                if (grid[r][c] == '1' && !visited[r][c]) {
-                    dfs(grid, visited, r, c);
+                if (grid[r][c] == '1') {
+                    dfs(grid, r, c);
                     islands++;
                 }
             }
@@ -50,22 +48,22 @@ public class NumberOfIslands200 {
 
     }
 
-    private static void dfs(char[][] grid, boolean[][] visited, int r, int c) {
+    private static void dfs(char[][] grid, int r, int c) {
 
         if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
             return;
         }
 
-        if (grid[r][c] == '0' || visited[r][c]) {
+        if (grid[r][c] == '0' || grid[r][c] == 'v') {
             return;
         }
 
-        visited[r][c] = true;
+        grid[r][c] = 'v';
 
-        dfs(grid, visited, r, c+1);
-        dfs(grid, visited, r, c-1);
-        dfs(grid, visited, r+1, c);
-        dfs(grid, visited, r-1, c);
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
+        dfs(grid, r+1, c);
+        dfs(grid, r-1, c);
 
     }
 
